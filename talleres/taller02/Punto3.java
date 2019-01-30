777976dbc04de93dd0ba9c39759bd924843c8da9
ESTPALREN <epalacior@eafit.edu.co>
2019-01-30 12:54:48 -0500

    Update Punto3.java

--- talleres/taller02/Punto3.java
+++ talleres/taller02/Punto3.java
@@ -1,14 +1,16 @@
 
 public class Punto3
 {
-    public static void palabra(String prefix, String s) {
-        int a = s.length();
-        if (a == 0){
-            System.out.println(prefix);
-        }
-        else {
-            for (int i=0;i<a;i++)
-                palabra(prefix + s.charAt(i), s.substring(0,i) + s.substring(i+1,a));
+    public static void Palabra(String s){
+        aux("",s);
+    }
+    
+    public static void aux(String entry,String s){
+    
+        if(s.length()==0){System.out.println(entry);
+        }else{
+        aux(entry+s.charAt(0),s.substring(1));
+        aux(entry,s.substring(1));
         }
     }
 }
