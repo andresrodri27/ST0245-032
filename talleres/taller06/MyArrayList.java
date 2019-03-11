d7c4a91731157637a862ad9ec4f28a961587cbe7
andresrodri27 <46932936+andresrodri27@users.noreply.github.com>
2019-03-11 14:57:37 -0500

    Update MyArrayList.java

--- talleres/taller06/MyArrayList.java
+++ talleres/taller06/MyArrayList.java
@@ -1,78 +1,50 @@
 
 import java.util.Arrays;
+public class myArrayList{
+    public int elements[];
+    public static final int DEFAULT_CAPACITY = 10;
+    public int size;
+ 
 
-/**
- * La clase MiArrayList tiene la intención de representar un objeto que simule el comportamiento
- * de la clase ya implementada "ArrayList"
- * es claro que no se puede utilizar dicha estructura ya utilizada dentro de este ejercicio. 
- * Para más información de la clase ArrayList:
- * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html"> Ver documentacion ArrayList </a>
- * 
- * 
- * @author Mauricio Toro, Andres Paez
- * @version 1
- */
-
-public class MyArrayList {
-    private int size;
-    private static final int DEFAULT_CAPACITY = 10;
-    private int elements[]; 
-
-    /**
-     * El metodo constructor se utiliza para incializar
-     * variables a valores neutros como 0 o null.
-     * El contructor no lleva parámetros en este caso.
-     */
-    public MyArrayList() {
+    public myArrayList(){
+        size = 0;
         elements = new int[DEFAULT_CAPACITY];
+    }
 
-    }     
-
-    /**
-     * Tiene la intención de retornar la longitud del objeto
-     * @return longitud del objeto
-     *
-     * El size esta influenciado por las funciones add y del
-     */
-    public int size() {
+    public int size(){
         return size;
-    }   
+    }
 
-    /** 
-     * @param e el elemento a guardar
-     * Agrega un elemento e a la última posición de la lista
-     *
-     */
-    public void add(int e) { 
-        if(size<DEFAULT_CAPACITY){
-           elements[size] = e;
-           size++;
-        }
-    }    
+    public void add(int a) {
+        elements[size]=a;
+        size = size+1;
+    }
 
-    private void grow(){
-        if (elements.length - size < 1){
-            elements = new Myarraylist.elements[size*2];
+    public void addOutOfBounds(int index, int a) throws ArrayIndexOutOfBoundsException{
+        if(index<=size) {
+            for(int i = size+1;i>index;i--) {
+                elements[i]=elements[i-1];
+            }
+            elements[index]=a;
+        }else {
+            throw new ArrayIndexOutOfBoundsException("Index overpass capacity");
         }
     }
-    
-    /**x
-     * @param index es la posicion en la cual se va agregar el elemento
-     * @param e el elemento a guardar
-     * Agrega un elemento e en la posición index de la lista
-     *
-     */
-    public void add(int index, int e) {
-        size++;
-    } 
 
-    /**
-     * @param index es la posicion en la cual se va agregar el elemento
-     *
-     * ELimina el elemento  en la posición index de la lista
-     *
-     */
-    public void del(int index){
-        size--;
+    public void createNewArray(int index,int a) throws ArrayIndexOutOfBoundsException{
+        if(index== elements.length+1){
+            elements = new int[DEFAULT_CAPACITY*2];
+            elements[index]=a;
+        }else{
+            throw new ArrayIndexOutOfBoundsException("Index overpass capacity");
+        }
+    }
+
+    public void deleteFromArray(int index){ 
+        elements[index]=0;
+        for(int i=index;i<size-1;i++){
+            elements[i]=elements[i+1];
+        }
+        size=size-1;   
     }
 }
