40d6fd134684b2b366adcae4cddf5ac1ace2ff2e
andresrodri27 <46932936+andresrodri27@users.noreply.github.com>
2019-03-27 17:21:26 -0500

    Update Sort.java

--- laboratorios/lab02/codigo/Sort.java
+++ laboratorios/lab02/codigo/Sort.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 import java.util.Random;
-public class Sort
+public class Lab2
 {
-    //MergeSort
+
     public static void main (int n){
         long start = System.currentTimeMillis();
-        int [] melo = generateArray(n);
-        Sort ob = new Sort(); 
-        ob.sort(melo, 0, melo.length-1);
+        int [] index = generateArray(n);
+        Lab2 ob = new Lab2(); 
+        ob.sort(index, 0, index.length-1);
         long end = System.currentTimeMillis() - start;
 
         System.out.println(end);
@@ -15,98 +15,68 @@ public class Sort
 
     private static int [] generateArray(int n){
         Random r = new Random();
-
         int [] integers = new int [n];
         for(int i=0; i<n ; i++){
             integers[i] = r.nextInt(100);
         }
-
         return integers;
     }
-    private static void merge(int melo[], int a, int m, int c) 
-    { 
-        // Find sizes of two subarrays to be merged 
-        int n1 = m - a + 1; 
-        int n2 = c - m; 
-  
-        /* Create temp arrays */
-        int L[] = new int [n1]; 
-        int R[] = new int [n2]; 
-  
-        /*Copy data to temp arrays*/
-        for (int i=0; i<n1; ++i){
-            L[i] = melo[a + i]; 
+
+    private static void merge(int index[], int a, int m, int c) { 
+        int subIndex1 = m - a + 1; 
+        int subIndex2 = c - m; 
+        int X[] = new int [subIndex1]; 
+        int Z[] = new int [subIndex2];
+        for (int i=0; i<subIndex1; ++i){
+            X[i] = index[a + i]; 
         }
-        for (int j=0; j<n2; ++j){ 
-            R[j] = melo[m + 1+ j];
+        for (int j=0; j<subIndex2; ++j){ 
+            Z[j] = index[m + 1+ j];
         }
-  
-  
-        /* Merge the temp arrays */
-  
-        // Initial indexes of first and second subarrays 
         int i = 0, j = 0; 
-  
-        // Initial index of merged subarry array 
         int k = a; 
-        while (i < n1 && j < n2) 
+        while (i < subIndex1 && j < subIndex2) 
         { 
-            if (L[i] <= R[j]) { 
-                melo[k] = L[i]; 
+            if (X[i] <= Z[j]) { 
+                index[k] = X[i]; 
                 i++; 
             } 
             else{ 
-                melo[k] = R[j]; 
+                index[k] = Z[j]; 
                 j++; 
             } 
             k++; 
         } 
-  
-        /* Copy remaining elements of L[] if any */
-        while (i < n1) { 
-            melo[k] = L[i]; 
+        while (i < subIndex1) { 
+            index[k] = X[i]; 
             i++; 
             k++; 
         } 
-  
-        /* Copy remaining elements of R[] if any */
-        while (j < n2) { 
-            melo[k] = R[j]; 
+        while (j < subIndex2) { 
+            index[k] = Z[j]; 
             j++; 
             k++; 
         } 
     } 
-  
-    // Main function that sorts arr[l..r] using 
-    // merge() 
-    private static void sort(int melo[], int a, int c) 
-    { 
-        if (a < c) 
-        { 
-            // Find the middle point 
+
+    private static void sort(int index[], int a, int c) { 
+        if (a < c){ 
             int m = (a+c)/2; 
-  
-            // Sort first and second halves 
-            sort(melo, a, m); 
-            sort(melo, m+1, c); 
-  
-            // Merge the sorted halves 
-            merge(melo, a, m, c); 
+            sort(index, a, m); 
+            sort(index, m+1, c); 
+            merge(index, a, m, c); 
         } 
     }
-    
-    //Insertion sort
-    public static void main2 (int n){
-        long start = System.currentTimeMillis();
-        int [] melo = generateArray(n);
-        arreglar(melo);
-        long end = System.currentTimeMillis() - start;
 
+    public static void maisubIndex2 (int n){
+        long start = System.currentTimeMillis();
+        int [] index = generateArray(n);
+        arregXar(index);
+        long end = System.currentTimeMillis() - start;
         System.out.println(end);
     }
 
-    private static void arreglar(int [] integers){
-
+    private static void arregXar(int [] integers){
         for(int i = 0; i <= integers.length-1; i++){
             for(int j = i+1; j < integers.length; j++){
                 if(integers[i]>integers[j]){
@@ -116,7 +86,7 @@ public class Sort
                 }
             }
         }
-
     }
 }
+
 
